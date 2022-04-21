1426b91b10ed764b68075ff4d79a4ed5671a2258
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-21 21:33:36 +0300

    imported BrowserRouter for creating Routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
