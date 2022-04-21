7d0549a6a9c6365033395bc3437b37cf61c0e4f9
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-21 23:04:57 +0300

    made markup for header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from 'react';
+
+import login_logo from '../../images/login_logo.png';
+import shop_bucket from '../../images/shop_bucket.png';
+import css from './Header.module.css';
+
+const Header: FC = () => {
+  return (
+    <div className={css.header_wrap}>
+      <div className={css.left_side}>
+        <h2>Каталог</h2>
+        <h2>Блог</h2>
+        <h2>Про нас</h2>
+      </div>
+
+      <div className={css.right_side}>
+        <h2>Контакти</h2>
+        <div className={css.login_logo}>
+          <img src={login_logo} alt="login_logo" />
+        </div>
+        <div className={css.shop_bucket}>
+          <img src={shop_bucket} alt="shop_bucket" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
