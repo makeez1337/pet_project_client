15646c991e828b9cf50880c7d761220a23ddae9c
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-23 19:54:05 +0300

    implemented slider component on page with discount products

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -2,12 +2,14 @@ import React, { FC } from 'react';
 
 import Header from '../../components/Header/Header';
 import ServiceMenu from '../../components/ServiceMenu/ServiceMenu';
+import DiscountProducts from '../../components/DiscountProducts/DiscountProducts';
 
 const IndexPage: FC = () => {
   return (
     <div>
       <Header />
       <ServiceMenu />
+      <DiscountProducts/>
     </div>
   );
 };
