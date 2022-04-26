b230ddea7287cd6ecac60e5e4a3b616c427ea1d5
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-26 15:28:50 +0300

    added reducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import loginPromptReducer from './slices/loginPromptSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    loginPromptReducer,
+  },
 })
 
 export type RootState = ReturnType<typeof store.getState>
