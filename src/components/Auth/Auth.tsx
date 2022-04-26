37ed9679e7696a46bc6205c7a5f7ded401067453
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-26 20:59:53 +0300

    added logic for background linear

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+
 import LoginPrompt from '../LoginPrompt/LoginPrompt';
 import RegistrationPrompt from '../RegistrationPrompt/RegistrationPrompt';
+import css from './Auth.module.css';
+import {useAppSelector} from "../../hooks/reduxHooks";
 
 const Auth = () => {
+    const {isLoginPromptOnScreen,isRegistrationPromptOnScreen} = useAppSelector(state => state.authPromptReducer);
   return (
-    <div>
+    <div className={isRegistrationPromptOnScreen || isLoginPromptOnScreen ? css.auth_wrap_active : ''}>
       <LoginPrompt/>
       <RegistrationPrompt/>
     </div>
