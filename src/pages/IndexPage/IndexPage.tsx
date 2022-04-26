cf76362a50b60314410b1ff2636c094708d97066
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-26 18:05:21 +0300

    implemented component Auth for logination&registration

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -3,13 +3,15 @@ import React, { FC } from 'react';
 import Header from '../../components/Header/Header';
 import ServiceMenu from '../../components/ServiceMenu/ServiceMenu';
 import DiscountProducts from '../../components/DiscountProducts/DiscountProducts';
+import Auth from '../../components/Auth/Auth';
 
 const IndexPage: FC = () => {
   return (
     <div>
       <Header />
       <ServiceMenu />
-      <DiscountProducts/>
+      <DiscountProducts />
+      <Auth/>
     </div>
   );
 };
