31c398841762ab4fb195985bd30b01346f1ac14f
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-26 15:30:35 +0300

    made function to open login prompt

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,18 @@
 import React, { FC } from 'react';
 
+import { useAppDispatch } from '../../hooks/reduxHooks';
+import { openLoginPrompt } from '../../store/slices/loginPromptSlice';
 import login_logo from '../../images/login_logo.png';
 import shop_bucket from '../../images/shop_bucket.png';
 import css from './Header.module.css';
 
 const Header: FC = () => {
+  const dispatch = useAppDispatch();
+
+  const openLogin = () => {
+    dispatch(openLoginPrompt(true));
+  };
+
   return (
     <div className={css.header_wrap}>
       <div className={css.left_side}>
@@ -17,11 +25,13 @@ const Header: FC = () => {
         <h2>Контакти</h2>
         <div className={css.login_logo}>
           <img src={login_logo} alt="login_logo" />
-          <div>Увійти</div>
+          <div className={css.login} onClick={openLogin}>
+            Увійти
+          </div>
         </div>
-        <div className={css.shop_bucket}>
+        <div className={css.shop_bucket_image}>
           <img src={shop_bucket} alt="shop_bucket" />
-          <div>Корзина</div>
+          <div className={css.shop_bucket}>Корзина</div>
         </div>
       </div>
     </div>
