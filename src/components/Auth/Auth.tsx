62bd63a5cab3ab0eee6ee20bacd017711a87e4e0
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-27 11:26:47 +0300

    reformatted code

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -3,14 +3,17 @@ import React from 'react';
 import LoginPrompt from '../LoginPrompt/LoginPrompt';
 import RegistrationPrompt from '../RegistrationPrompt/RegistrationPrompt';
 import css from './Auth.module.css';
-import {useAppSelector} from "../../hooks/reduxHooks";
+import { useAppSelector } from '../../hooks/reduxHooks';
 
 const Auth = () => {
-    const {isLoginPromptOnScreen,isRegistrationPromptOnScreen} = useAppSelector(state => state.authPromptReducer);
+  const { isLoginPromptOnScreen, isRegistrationPromptOnScreen } = useAppSelector(
+    (state) => state.authPromptReducer
+  );
   return (
-    <div className={isRegistrationPromptOnScreen || isLoginPromptOnScreen ? css.auth_wrap_active : ''}>
-      <LoginPrompt/>
-      <RegistrationPrompt/>
+    <div
+      className={isRegistrationPromptOnScreen || isLoginPromptOnScreen ? css.auth_wrap_active : ''}>
+      <LoginPrompt />
+      <RegistrationPrompt />
     </div>
   );
 };
