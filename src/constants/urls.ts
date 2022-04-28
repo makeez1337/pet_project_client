456c4f789078a9857011b6ae2951673c0213004c
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-28 22:59:17 +0300

    added endpoint for refresh

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -3,7 +3,8 @@ const baseURL: string = 'http://localhost:5200';
 export enum Urls {
   Auth = '/auth',
   Registration = '/registration',
-  Login = '/login'
+  Login = '/login',
+  Refresh = '/refresh'
 }
 
 export default baseURL;
