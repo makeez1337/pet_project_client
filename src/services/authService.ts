93745cc2fdb3d61b7afc606278ba703828194ccb
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-28 15:44:36 +0300

    typed service for login

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,9 +1,10 @@
 import { axiosService } from './axiosService';
 import { Urls } from '../constants/urls';
 import { ILoginCredentials, ILoginResponse } from '../interfaces/authInterface';
+import { AxiosResponse } from 'axios';
 
 export const authService = {
-  login: async (credentials: ILoginCredentials): Promise<ILoginResponse> => {
+  login: async (credentials: ILoginCredentials): Promise<AxiosResponse<ILoginResponse>> => {
     return axiosService.post(`${Urls.Auth}${Urls.Login}`, credentials);
   }
 };
