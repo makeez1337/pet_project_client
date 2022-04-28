4bb7ed67654bd6e378290bfac52efd5e5e20ccb6
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-28 23:02:16 +0300

    added service for refresh endpoint

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,10 +1,13 @@
 import { axiosService } from './axiosService';
 import { Urls } from '../constants/urls';
-import { ILoginCredentials, ILoginResponse } from '../interfaces/authInterface';
+import { ILoginCredentials, IAuthResponse } from '../interfaces/authInterface';
 import { AxiosResponse } from 'axios';
 
 export const authService = {
-  login: async (credentials: ILoginCredentials): Promise<AxiosResponse<ILoginResponse>> => {
+  login: async (credentials: ILoginCredentials): Promise<AxiosResponse<IAuthResponse>> => {
     return axiosService.post(`${Urls.Auth}${Urls.Login}`, credentials);
+  },
+  refresh: async (refreshToken: string):Promise<AxiosResponse<IAuthResponse>> => {
+    return axiosService.post(`${Urls.Auth}${Urls.Refresh}`, refreshToken);
   }
 };
