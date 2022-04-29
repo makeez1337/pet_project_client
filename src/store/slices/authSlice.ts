4fefae264d1691df86611f8da7004bf41d6295f5
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-29 12:03:09 +0300

    created logOutThunk, added in extraReducer with state fulfilled

--- src/store/slices/authSlice.ts
+++ src/store/slices/authSlice.ts
@@ -30,34 +30,53 @@ export const loginThunk = createAsyncThunk(
     } catch (e) {
       if (axios.isAxiosError(e)) {
         console.log('error message: ', e.response?.data);
-        return e.response?.data;
+        return rejectWithValue(e.response?.data);
       }
       if (e instanceof Error) {
         console.log('unexpected error: ', e.message);
-        return e.message;
+        return rejectWithValue(e.message);
       }
     }
   }
 );
 
 export const checkAuthThunk = createAsyncThunk(
-    'authSlice/checkAuthThunk',
-    async (_, {rejectWithValue}) => {
-      try {
-        const response = await authService.refresh();
-        localStorage.setItem('accessToken', response.data.accessToken);
-        return response.data;
-      } catch (e) {
-        if (axios.isAxiosError(e)) {
-          console.log('error message: ', e.response?.data);
-          return e.response?.data;
-        }
-        if (e instanceof Error) {
-          console.log('unexpected error: ', e.message);
-          return e.message;
-        }
+  'authSlice/checkAuthThunk',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await authService.refresh();
+      localStorage.setItem('accessToken', response.data.accessToken);
+      return response.data;
+    } catch (e) {
+      if (axios.isAxiosError(e)) {
+        console.log('error message: ', e.response?.data);
+        return rejectWithValue(e.response?.data);
+      }
+      if (e instanceof Error) {
+        console.log('unexpected error: ', e.message);
+        return rejectWithValue(e.message);
       }
     }
+  }
+);
+
+export const logOutThunk = createAsyncThunk(
+  'authSlice/logOutThunk',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await authService.logout();
+      localStorage.removeItem('accessToken');
+    } catch (e) {
+      if (axios.isAxiosError(e)) {
+        console.log('error message: ', e.response?.data);
+        return rejectWithValue(e.response?.data);
+      }
+      if (e instanceof Error) {
+        console.log('unexpected error: ', e.message);
+        return rejectWithValue(e.message);
+      }
+    }
+  }
 );
 
 export const authSlice = createSlice({
@@ -77,19 +96,25 @@ export const authSlice = createSlice({
       state.isLoginPromptOnScreen = true;
     }
   },
-  extraReducers: (builder => {
+  extraReducers: (builder) => {
     // loginThunk
-    builder.addCase(loginThunk.fulfilled, ((state, action) => {
+    builder.addCase(loginThunk.fulfilled, (state, action) => {
       state.isAuth = true;
-      state.user = action.payload.user;
-    }))
+      state.user = action.payload?.user as IUser;
+    });
 
     // checkAuthThunk
-    builder.addCase(checkAuthThunk.fulfilled, ((state, action) => {
+    builder.addCase(checkAuthThunk.fulfilled, (state, action) => {
       state.isAuth = true;
-      state.user = action.payload.user;
-    }))
-  })
+      state.user = action.payload?.user as IUser;
+    });
+
+    // logOutThunk
+    builder.addCase(logOutThunk.fulfilled, (state, action) => {
+      state.isAuth = false;
+      state.user = null;
+    })
+  }
 });
 
 export const { openLoginPrompt, openRegistrationPrompt, switchToLoginPrompt } = authSlice.actions;
