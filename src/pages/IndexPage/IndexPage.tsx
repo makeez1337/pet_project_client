dbd416ceb8a530502279073bbb4ce764b3cc355a
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-29 10:51:20 +0300

    added method check for auth

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,17 +1,27 @@
-import React, { FC } from 'react';
+import React, {FC, useEffect} from 'react';
 
 import Header from '../../components/Header/Header';
 import ServiceMenu from '../../components/ServiceMenu/ServiceMenu';
 import DiscountProducts from '../../components/DiscountProducts/DiscountProducts';
 import Auth from '../../components/Auth/Auth';
+import { useAppDispatch } from '../../hooks/reduxHooks';
+import {checkAuthThunk} from "../../store/slices/authSlice";
 
 const IndexPage: FC = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+      if (localStorage.getItem('accessToken')) {
+            dispatch(checkAuthThunk());
+      }
+  },[])
+
   return (
     <div>
       <Header />
       <ServiceMenu />
       <DiscountProducts />
-      <Auth/>
+      <Auth />
     </div>
   );
 };
