5edbbdc5b23908faab3e1d722e2c5ea6bb58b432
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-29 10:52:58 +0300

    added interceptor to catch accessToken

--- src/services/axiosService.ts
+++ src/services/axiosService.ts
@@ -2,4 +2,9 @@ import axios from 'axios';
 
 import baseURL from '../constants/urls';
 
-export const axiosService = axios.create({ baseURL });
+export const axiosService = axios.create({ baseURL, withCredentials: true });
+
+axiosService.interceptors.request.use((config: any) => {
+  config.headers.Authorization = `${localStorage.getItem('accessToken')}`;
+  return config;
+});
