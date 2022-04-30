535d7a79dcd765d10508048a7e0be1d27866f198
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-30 20:49:35 +0300

    added interseptor.response to handle 401(Unauthorized) error

--- src/services/axiosService.ts
+++ src/services/axiosService.ts
@@ -1,10 +1,31 @@
 import axios from 'axios';
 
-import baseURL from '../constants/urls';
+import baseURL, { Urls } from '../constants/urls';
+import { authService } from './authService';
 
 export const axiosService = axios.create({ baseURL, withCredentials: true });
 
 axiosService.interceptors.request.use((config: any) => {
-  config.headers.Authorization = `${localStorage.getItem('accessToken')}`;
+  config.headers.Authorization = localStorage.getItem('accessToken');
   return config;
 });
+
+axiosService.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  async (error) => {
+    const originalConfig = error.config;
+    try {
+      if (error?.response?.status === 401 && !originalConfig._retry) {
+        originalConfig._retry = true;
+        const response = await authService.refresh();
+        localStorage.setItem('accessToken', response.data.accessToken);
+        return axiosService.request(originalConfig);
+      }
+    } catch (e) {
+      console.log('Не авторизований');
+    }
+    throw error;
+  }
+);
