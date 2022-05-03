f1ab792bf8c8d0d9ef42947a44519aeeacf1bf73
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-03 11:26:27 +0300

    changed name of method

--- src/validators/auth/authValidator.ts
+++ src/validators/auth/authValidator.ts
@@ -3,7 +3,7 @@ import Joi from 'joi';
 import { commonValidator } from '../common/commonValidator';
 
 export const authValidator = {
-  registration: Joi.object().keys({
+  login: Joi.object().keys({
     email: commonValidator.emailValidator,
     password: commonValidator.passwordValidator
   })
