cea814a64b8963576d2d7feb68b0541fc02bcd2d
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-03 11:26:48 +0300

    added message for pattern error

--- src/validators/common/commonValidator.ts
+++ src/validators/common/commonValidator.ts
@@ -3,6 +3,8 @@ import Joi from 'joi';
 import { constants } from '../../constants/constants';
 
 export const commonValidator = {
-  emailValidator: Joi.string().pattern(constants.emailRegexp).trim().required(),
+  emailValidator: Joi.string().pattern(constants.emailRegexp).trim().required().messages({
+    'string.pattern.base': `"email" should include "@" and symbols after`
+  }),
   passwordValidator: Joi.string().alphanum().min(8).trim().required()
 };
