86155bffdc34cdc486c3368550f255eed8e91b43
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-03 12:41:18 +0300

    added validation for registration

--- src/validators/auth/authValidator.ts
+++ src/validators/auth/authValidator.ts
@@ -3,6 +3,15 @@ import Joi from 'joi';
 import { commonValidator } from '../common/commonValidator';
 
 export const authValidator = {
+  registration: Joi.object().keys({
+    firstName: Joi.string().min(1).trim().required(),
+    lastName: Joi.string().min(1).trim().required(),
+    email: commonValidator.emailValidator,
+    password: commonValidator.passwordValidator,
+    repeatedPassword: Joi.string().valid(Joi.ref('password')).required().messages({
+      'any.only': 'Passwords is not equal'
+    })
+  }),
   login: Joi.object().keys({
     email: commonValidator.emailValidator,
     password: commonValidator.passwordValidator
