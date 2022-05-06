6ce624b7ac8a564c48d19ff319125e24ad46960b
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-06 22:00:46 +0300

    added route for 'catalog/:page' endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import './index.css';
 import IndexPage from './pages/IndexPage/IndexPage';
+import Catalog from './pages/Catalog/Catalog';
 
 const App: FC = () => {
   return (
     <Routes>
       <Route index element={<IndexPage />} />
+
+      <Route path={'catalog/:page'} element={<Catalog />} />
     </Routes>
   );
 };
