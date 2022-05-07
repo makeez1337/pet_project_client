c600f64cae29250887b476a77bcc4d7bab224a84
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-07 12:35:04 +0300

    created memory map component

---
+++ src/components/MemoryMap/MemoryMap.tsx
@@ -0,0 +1,26 @@
+import React, { FC, useEffect, useState } from 'react';
+
+import { IMemory } from '../../interfaces/PhoneFieldsInterface';
+import Memory from '../Memory/Memory';
+import { memoryService } from '../../services/memoryService';
+import css from './MemoryMap.module.css';
+
+const MemoryMap: FC = () => {
+  const [memory, setMemory] = useState<IMemory[] | null>(null);
+
+  useEffect(() => {
+    memoryService.getAll().then((res) => setMemory(res.data));
+  }, []);
+
+  return (
+    <div className={css.memory_wrap}>
+      <hr />
+      <div className={css.filter_describe}>Вбудована пам'ять:</div>
+      {memory?.map((memory) => (
+        <Memory key={memory.id} {...memory} />
+      ))}
+    </div>
+  );
+};
+
+export default MemoryMap;
