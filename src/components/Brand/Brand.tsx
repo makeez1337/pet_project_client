0ff53de82c62805debe4fe46d146a292a5787f6b
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-07 11:09:51 +0300

    component for brand

---
+++ src/components/Brand/Brand.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+
+import { IBrand } from '../../interfaces/brandInterface';
+import css from './Brand.module.css';
+
+const Brand: FC<IBrand> = ({ name }) => {
+  return <div className={css.brand}>{name}</div>;
+};
+
+export default Brand;
