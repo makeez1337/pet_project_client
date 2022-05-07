82f7e616c28ca14f2b18b5e43ff3739a4b0122fe
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-07 11:11:56 +0300

    added some markup, created map for brands to drill value in component Brand

--- src/components/FilterMenu/FilterMenu.tsx
+++ src/components/FilterMenu/FilterMenu.tsx
@@ -1,21 +1,27 @@
-import React, {useEffect, useState} from 'react';
+import React, { FC, useEffect, useState } from 'react';
 
 import css from './FilterMenu.module.css';
-import {brandService} from "../../services/brandService";
-import {IBrand} from "../../interfaces/brandInterface";
+import { brandService } from '../../services/brandService';
+import { IBrand } from '../../interfaces/brandInterface';
+import Brand from '../Brand/Brand';
 
-const FilterMenu = () => {
+const FilterMenu: FC = () => {
+  const [brands, setBrands] = useState<IBrand[] | null>(null);
 
-  const [posts, setPosts] = useState<IBrand | null>(null);
-
-  useEffect( () => {
-     brandService.getAll().then(res => setPosts(res.data));
-  },[])
+  useEffect(() => {
+    brandService.getAll().then((res) => setBrands(res.data));
+  }, []);
 
   return (
     <div className={css.container}>
       <div>
-        <h1>Смартфони</h1>
+        <h1 className={css.header_style}>Смартфони</h1>
+        <div className={css.brands_wrap}>
+          <hr />
+          {brands?.map((brand) => (
+            <Brand key={brand.id} {...brand} />
+          ))}
+        </div>
       </div>
     </div>
   );
