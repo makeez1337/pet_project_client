2e3b7c85b9638d69d272554296aae499921c2392
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-07 12:36:09 +0300

    added interfaces for some tables that are associated with phone table

---
+++ src/interfaces/PhoneFieldsInterface.ts
@@ -0,0 +1,14 @@
+export interface IBrand {
+  id: number;
+  name: string;
+}
+
+export interface IRam {
+  id: number;
+  ram: number;
+}
+
+export interface IMemory {
+  id: number;
+  memory: number;
+}
