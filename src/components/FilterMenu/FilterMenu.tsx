dd3d77ce04790f431ec264e1757f68c185290559
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-07 12:32:55 +0300

    added two components in markup

--- src/components/FilterMenu/FilterMenu.tsx
+++ src/components/FilterMenu/FilterMenu.tsx
@@ -1,29 +1,17 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC } from 'react';
 
 import css from './FilterMenu.module.css';
-import { brandService } from '../../services/brandService';
-import { IBrand } from '../../interfaces/brandInterface';
-import Brand from '../Brand/Brand';
+import Brands from '../Brands/Brands';
+import MemoryMap from "../MemoryMap/MemoryMap";
 
 const FilterMenu: FC = () => {
-  const [brands, setBrands] = useState<IBrand[] | null>(null);
-
-  useEffect(() => {
-    brandService.getAll().then((res) => setBrands(res.data));
-  }, []);
 
   return (
     <div className={css.container}>
-      <div>
-        <h1 className={css.header_style}>Смартфони</h1>
-        <div className={css.brands_wrap}>
-          <hr />
-          <div className={css.filter_describe}>Модель:</div>
-          {brands?.map((brand) => (
-            <Brand key={brand.id} {...brand} />
-          ))}
-        </div>
-      </div>
+      <h1 className={css.header_style}>Смартфони</h1>
+      <Brands />
+      <MemoryMap/>
+
     </div>
   );
 };
