820466fbbb57db9be18f5ba504649b1f19802eb0
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-07 10:13:10 +0300

    wrapped filter menu by div

--- src/pages/Catalog/Catalog.tsx
+++ src/pages/Catalog/Catalog.tsx
@@ -3,12 +3,16 @@ import Header from '../../components/Header/Header';
 import Auth from '../../components/Auth/Auth';
 import FilterMenu from '../../components/FilterMenu/FilterMenu';
 
+import css from './Catalog.module.css';
+
 const Catalog = () => {
   return (
     <div>
       <Header />
       <Auth />
-      <FilterMenu />
+      <div className={css.menu_wrap}>
+        <FilterMenu />
+      </div>
     </div>
   );
 };
