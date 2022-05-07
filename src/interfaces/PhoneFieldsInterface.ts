c5b5426193c5d5b99e78da4c377e2e476d117f1c
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-07 20:23:07 +0300

    added new interfaces

--- src/interfaces/PhoneFieldsInterface.ts
+++ src/interfaces/PhoneFieldsInterface.ts
@@ -1,3 +1,24 @@
+export interface IPhone {
+  id: number;
+  name: string;
+  description: string;
+  memoryId: number;
+  ramId: number;
+  camera: number;
+  price: number;
+  img: string;
+  brandId: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface PhonePaginationResponse {
+  page: number;
+  perPage: number;
+  count: number;
+  rows: IPhone[];
+}
+
 export interface IBrand {
   id: number;
   name: string;
