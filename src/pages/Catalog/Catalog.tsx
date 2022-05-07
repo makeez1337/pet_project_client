aeeba9d08a795ffa2b6ebf46f0e7da0031871a96
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-07 22:02:22 +0300

    added check for auth

--- src/pages/Catalog/Catalog.tsx
+++ src/pages/Catalog/Catalog.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../../components/Header/Header';
 import Auth from '../../components/Auth/Auth';
 import FilterMenu from '../../components/FilterMenu/FilterMenu';
 
 import css from './Catalog.module.css';
 import Phones from '../../components/Phones/Phones';
+import { useAppDispatch } from '../../hooks/reduxHooks';
+import { checkAuthThunk } from '../../store/slices/authSlice';
 
 const Catalog = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(checkAuthThunk());
+  });
+
   return (
     <div>
       <Header />
