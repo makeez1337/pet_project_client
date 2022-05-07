02e98e0bb6e5fcf8ba1ff4d265d401abed94b932
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-07 13:36:09 +0300

    added component RamMap

--- src/components/FilterMenu/FilterMenu.tsx
+++ src/components/FilterMenu/FilterMenu.tsx
@@ -1,17 +1,17 @@
 import React, { FC } from 'react';
 
-import css from './FilterMenu.module.css';
 import Brands from '../Brands/Brands';
-import MemoryMap from "../MemoryMap/MemoryMap";
+import MemoryMap from '../MemoryMap/MemoryMap';
+import RamMap from '../RamMap/RamMap';
+import css from './FilterMenu.module.css';
 
 const FilterMenu: FC = () => {
-
   return (
     <div className={css.container}>
       <h1 className={css.header_style}>Смартфони</h1>
       <Brands />
-      <MemoryMap/>
-
+      <MemoryMap />
+      <RamMap />
     </div>
   );
 };
