5fa61769d0ea32eb4fa240cd615c31e0cf6c680d
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-07 13:37:17 +0300

    component ram map, getting all rams from service and drill response in component deeper

---
+++ src/components/RamMap/RamMap.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+
+import Ram from '../Ram/Ram';
+import { IRam } from '../../interfaces/PhoneFieldsInterface';
+import { ramService } from '../../services/ramService';
+import css from './RamMap.module.css';
+
+const RamMap = () => {
+  const [ram, setRam] = useState<IRam[] | null>(null);
+
+  useEffect(() => {
+    ramService.getAll().then((res) => setRam(res.data));
+  }, []);
+
+  return (
+    <div className={css.ram_wrap}>
+      <hr />
+      <div className={css.filter_describe}>Оперативна пам'ять:</div>
+      {ram?.map((ram) => (
+        <Ram key={ram.id} {...ram} />
+      ))}
+    </div>
+  );
+};
+
+export default RamMap;
