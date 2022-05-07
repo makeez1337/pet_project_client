1ed12f91ecec6c1eb4c6a91079d0cf5132259b8c
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-07 13:36:32 +0300

    component of every ram

---
+++ src/components/Ram/Ram.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+
+import { IRam } from '../../interfaces/PhoneFieldsInterface';
+import css from './Ram.module.css';
+
+const Ram: FC<IRam> = ({ ram }) => {
+  return <div className={css.ram}>{ram}</div>;
+};
+
+export default Ram;
