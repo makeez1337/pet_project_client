05c0cc040e8094450a62db7c583387e3cf93fbdd
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-07 10:14:13 +0300

    added new endpoints in enum

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -2,6 +2,9 @@ const baseURL: string = 'http://localhost:5200';
 
 export enum Urls {
   Auth = '/auth',
+  Brands = '/brands',
+  Ram = '/ram',
+  Memory = '/memory',
   Registration = '/registration',
   Login = '/login',
   Refresh = '/refresh',
