f2a6f11d59f8933988468af80b7ee164e2493c56
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-08 11:58:44 +0300

    formatted code

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,20 +1,20 @@
-import React, {FC, useEffect} from 'react';
+import React, { FC, useEffect } from 'react';
 
 import Header from '../../components/Header/Header';
 import ServiceMenu from '../../components/ServiceMenu/ServiceMenu';
 import DiscountProducts from '../../components/DiscountProducts/DiscountProducts';
 import Auth from '../../components/Auth/Auth';
 import { useAppDispatch } from '../../hooks/reduxHooks';
-import {checkAuthThunk} from "../../store/slices/authSlice";
+import { checkAuthThunk } from '../../store/slices/authSlice';
 
 const IndexPage: FC = () => {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-      if (localStorage.getItem('accessToken')) {
-            dispatch(checkAuthThunk());
-      }
-  },[])
+    if (localStorage.getItem('accessToken')) {
+      dispatch(checkAuthThunk());
+    }
+  }, []);
 
   return (
     <div>
