96484a4c540192d88b49ed9e98e82a9c96f10e3f
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-08 11:59:32 +0300

    added function to add memory for filter

--- src/components/Memory/Memory.tsx
+++ src/components/Memory/Memory.tsx
@@ -2,9 +2,26 @@ import React, { FC } from 'react';
 
 import { IMemory } from '../../interfaces/PhoneFieldsInterface';
 import css from './Memory.module.css';
+import { useAppDispatch, useAppSelector } from '../../hooks/reduxHooks';
+import { addMemory } from '../../store/slices/filterSlice';
 
-const Memory: FC<IMemory> = ({ memory }) => {
-  return <div className={css.text_wrap}><span className={css.memory}>{memory} Gb</span></div>;
+const Memory: FC<IMemory> = ({ id, memory }) => {
+  const dispatch = useAppDispatch();
+  const { activeMemory } = useAppSelector((state) => state.filterReducer);
+
+  const addMemoryFilter = () => {
+    dispatch(addMemory(id));
+  };
+
+  const isActive = activeMemory.includes(id);
+
+  return (
+    <div className={css.text_wrap}>
+      <span className={isActive ? css.active : css.memory} onClick={addMemoryFilter}>
+        {memory} Gb
+      </span>
+    </div>
+  );
 };
 
 export default Memory;
