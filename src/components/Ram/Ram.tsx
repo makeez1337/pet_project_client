dbaa5fbc244a1a5eab8c31bd9c062a8e3ef22c2c
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-08 11:59:47 +0300

    added function to add ram for filter

--- src/components/Ram/Ram.tsx
+++ src/components/Ram/Ram.tsx
@@ -2,9 +2,26 @@ import React, { FC } from 'react';
 
 import { IRam } from '../../interfaces/PhoneFieldsInterface';
 import css from './Ram.module.css';
+import { useAppDispatch, useAppSelector } from '../../hooks/reduxHooks';
+import { addRam } from '../../store/slices/filterSlice';
 
-const Ram: FC<IRam> = ({ ram }) => {
-  return <div className={css.text_wrap}><span className={css.ram}>{ram} Gb</span></div>;
+const Ram: FC<IRam> = ({ id, ram }) => {
+  const dispatch = useAppDispatch();
+  const { activeRam } = useAppSelector((state) => state.filterReducer);
+
+  const addRamFilter = () => {
+    dispatch(addRam(id));
+  };
+
+  const isActive = activeRam.includes(id);
+
+  return (
+    <div className={css.text_wrap}>
+      <span className={isActive ? css.active : css.ram} onClick={addRamFilter}>
+        {ram} Gb
+      </span>
+    </div>
+  );
 };
 
 export default Ram;
