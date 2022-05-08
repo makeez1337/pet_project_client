1d5703fda58a5b60eef6134797c2f011621690c7
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-08 11:57:56 +0300

    added function to add active brand for filter

--- src/components/Brand/Brand.tsx
+++ src/components/Brand/Brand.tsx
@@ -2,9 +2,26 @@ import React, { FC } from 'react';
 
 import { IBrand } from '../../interfaces/PhoneFieldsInterface';
 import css from './Brand.module.css';
+import { useAppDispatch, useAppSelector } from '../../hooks/reduxHooks';
+import { addBrand } from '../../store/slices/filterSlice';
 
-const Brand: FC<IBrand> = ({ name }) => {
-  return <div className={css.text_wrap}><span className={css.brand}>{name}</span></div>;
+const Brand: FC<IBrand> = ({ id, name }) => {
+  const dispatch = useAppDispatch();
+  const { activeBrand } = useAppSelector((state) => state.filterReducer);
+
+  const addBrandFilter = () => {
+    dispatch(addBrand(id));
+  };
+
+  const isActive = activeBrand.includes(id);
+
+  return (
+    <div className={css.text_wrap}>
+      <span className={isActive ? css.active : css.brand} onClick={addBrandFilter}>
+        {name}
+      </span>
+    </div>
+  );
 };
 
 export default Brand;
