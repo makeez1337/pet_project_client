ea76e59020031fa304231349fbba0b0aa18f2a30
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-08 11:59:54 +0300

    added filterReducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+
 import authReducer from './slices/authSlice';
+import filterReducer from './slices/filterSlice';
 
 export const store = configureStore({
   reducer: {
     authReducer,
-  },
-})
+    filterReducer
+  }
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
