34dc4f4c7f291dadcaee3b4ac1f6e0f83de239c4
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-09 22:08:00 +0300

    changed name of method

--- src/services/phoneService.ts
+++ src/services/phoneService.ts
@@ -5,7 +5,7 @@ import { AxiosResponse } from 'axios';
 import { PhonePaginationResponse } from '../interfaces/PhoneFieldsInterface';
 
 export const phoneService = {
-  getByPage: async (searchQuery: string): Promise<AxiosResponse<PhonePaginationResponse>> => {
+  getByQuery: async (searchQuery: string): Promise<AxiosResponse<PhonePaginationResponse>> => {
     return axiosService.get(`${Urls.Phones}${searchQuery}`);
   }
 };
