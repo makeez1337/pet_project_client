2f1f9943a1dbc4ff2a7cc643a070e9d1ac5d4b83
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-09 21:44:39 +0300

    removed filterReducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import authReducer from './slices/authSlice';
-import filterReducer from './slices/filterSlice';
 
 export const store = configureStore({
   reducer: {
-    authReducer,
-    filterReducer
+    authReducer
   }
 });
 
