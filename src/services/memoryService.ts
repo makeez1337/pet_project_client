287bf3a9d3fe8be4be02287eb1ea7fc855414a87
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-11 01:03:04 +0300

    formatted code

--- src/services/memoryService.ts
+++ src/services/memoryService.ts
@@ -1,8 +1,8 @@
 import { AxiosResponse } from 'axios';
 
 import axiosService from './axiosService';
-import { Urls } from '../constants/urls';
-import { IMemory } from '../interfaces/PhoneFieldsInterface';
+import { Urls } from '../constants';
+import { IMemory } from '../interfaces';
 
 export const memoryService = {
   getAll: async (): Promise<AxiosResponse<IMemory[]>> => axiosService.get(Urls.Memory)
