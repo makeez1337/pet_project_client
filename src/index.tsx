b799d87e47b521b52055e175509fbcf1bbd3d8a0
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-11 01:02:29 +0300

    formatted code

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import App from './App';
-import { store } from './store/store';
+import { store } from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
