725a6325f7a9e4a3f5af5335118f013061f2b9c8
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-10 19:25:50 +0300

    formatted lines, deleted unused hook useLocation

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useEffect } from 'react';
 
 import css from './Pagination.module.css';
-import {useLocation, useSearchParams} from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 
 interface PaginationProps {
   currentPage: null | number;
@@ -11,8 +11,6 @@ interface PaginationProps {
 
 const Pagination: FC<PaginationProps> = ({ totalPages, currentPage, perPage }) => {
   const [searchParams, setSearchParams] = useSearchParams();
-
-  const {search} = useLocation();
 
   const gte = searchParams.get('gte') || '0';
   const lte = searchParams.get('lte') || '45999';
@@ -40,17 +38,17 @@ const Pagination: FC<PaginationProps> = ({ totalPages, currentPage, perPage }) =
     if (totalPages && perPage === 0) {
       setSearchParams({ page: '1', ...setParamsObj });
     }
-  },[totalPages, perPage]);
+  }, [totalPages, perPage]);
 
   return (
     <div className={css.content_wrap}>
       <button
         onClick={moveToPreviousPage}
-        disabled={!!isPageFirst || !totalPages}
+        disabled={isPageFirst || !totalPages}
         className={css.btn_style}>
         ПОПЕРЕДНЯ СТОРІНКА
       </button>
-      <button onClick={moveToNextPage} disabled={!!isPageLast} className={css.btn_style}>
+      <button onClick={moveToNextPage} disabled={isPageLast} className={css.btn_style}>
         НАСТУПНА СТОРІНКА
       </button>
     </div>
