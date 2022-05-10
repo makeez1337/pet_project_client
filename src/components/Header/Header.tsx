d9de748ad506cb12696d2730425f3e34304bcb66
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-10 11:39:13 +0300

    formatted 2nd line

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import {Link, useLocation} from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import { useAppDispatch, useAppSelector } from '../../hooks/reduxHooks';
 import { logOutThunk, openLoginPrompt } from '../../store/slices/authSlice';
