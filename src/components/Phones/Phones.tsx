22fd23bdca33de1a12da80d2c945d530666d43a6
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-10 16:26:57 +0300

    added handling is phones loading, added pagination component

--- src/components/Phones/Phones.tsx
+++ src/components/Phones/Phones.tsx
@@ -5,21 +5,41 @@ import { phoneService } from '../../services/phoneService';
 import { IPhone } from '../../interfaces/PhoneFieldsInterface';
 import Phone from '../Phone/Phone';
 import css from './Phones.module.css';
+import Pagination from '../Pagination/Pagination';
 
 const Phones: FC = () => {
   const [phones, setPhones] = useState<IPhone[] | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const [currentPage, setCurrentPage] = useState<number | null>(null);
+  const [totalPages, setTotalPages] = useState<number | null>(null);
+  const [perPage, setPerPage] = useState<number | null>(null);
 
   const { search } = useLocation();
 
   useEffect(() => {
-    phoneService.getByQuery(search).then((res) => setPhones(res.data.rows));
+    setIsLoading(true);
+    phoneService.getByQuery(search || '').then((res) => {
+      setCurrentPage(res.data.page);
+      setTotalPages(res.data.totalPages);
+      setPerPage(res.data.perPage);
+      setPhones(res.data.rows);
+      setIsLoading(false);
+    });
   }, [search]);
+
 
   return (
     <div className={css.content_wrap}>
+      {isLoading ? (
+        <div className={css.loading_wrap}>
+          <h1 className={css.loading}>LOADING...</h1>
+        </div>
+      ) : null}
       {phones?.map((phone) => (
         <Phone key={phone.id} {...phone} />
       ))}
+      <Pagination currentPage={currentPage} totalPages={totalPages} perPage={perPage} />
     </div>
   );
 };
