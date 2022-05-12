4e915c95c8c76d7acc8ea00eb37a51ee75658e00
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-12 10:40:08 +0300

    added route for endpoint admin

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import './index.css';
 import IndexPage from './pages/IndexPage/IndexPage';
 import Catalog from './pages/Catalog/Catalog';
+import Admin from './pages/Admin/Admin';
 
 const App: FC = () => {
   return (
@@ -11,6 +12,8 @@ const App: FC = () => {
       <Route index element={<IndexPage />} />
 
       <Route path={'catalog'} element={<Catalog />} />
+
+      <Route path={'admin'} element={<Admin />} />
     </Routes>
   );
 };
