69d85d00ab7c0a08f5b7e2b539ab24eddbca2185
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-12 22:57:19 +0300

    added regexp field to make numbers with commas

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,5 +1,6 @@
 export const constants = {
   emailRegexp: /^\S+@\S+\.\S+$/,
 
-  baseImgUrl: 'http://localhost:5200/phoneImg/'
+  baseImgUrl: 'http://localhost:5200/phoneImg/',
+  numberWithCommas: /\B(?=(\d{3})+(?!\d))/g,
 };
