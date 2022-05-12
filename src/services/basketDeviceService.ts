9b2aa5aea91d773d49a6c37343dc0f0b6fc974a8
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-12 22:50:28 +0300

    created method to get basket devices by user id

--- src/services/basketDeviceService.ts
+++ src/services/basketDeviceService.ts
@@ -2,10 +2,13 @@ import axiosService from './axiosService';
 import { AxiosResponse } from 'axios';
 
 import { Urls } from '../constants';
-import { IBasketDeviceResponse } from '../interfaces';
+import { IBasketDeviceResponse, IBasketDevicesCountResponse } from '../interfaces';
 
 export const basketDeviceService = {
   createBasketDevice: async (phoneId: number): Promise<AxiosResponse<IBasketDeviceResponse>> => {
     return axiosService.post(`${Urls.BasketDevice}/addItem`, { phoneId });
+  },
+  getDevicesByUserId: (userId: number): Promise<AxiosResponse<IBasketDevicesCountResponse[]>> => {
+    return axiosService.get(`${Urls.BasketDevice}/${userId}`);
   }
 };
