ecc1d620b32c58151c587643cdb04bf5a734ff2d
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-12 10:40:56 +0300

    added method to create phone

--- src/services/phoneService.ts
+++ src/services/phoneService.ts
@@ -2,10 +2,13 @@ import axiosService from './axiosService';
 
 import { Urls } from '../constants';
 import { AxiosResponse } from 'axios';
-import { PhonePaginationResponse } from '../interfaces';
+import { IPhone, PhonePaginationResponse } from '../interfaces';
 
 export const phoneService = {
   getByQuery: async (searchQuery: string): Promise<AxiosResponse<PhonePaginationResponse>> => {
     return axiosService.get(`${Urls.Phones}${searchQuery}`);
+  },
+  create: async (data: FormData): Promise<AxiosResponse<IPhone>> => {
+    return axiosService.post(`${Urls.Phones}`, data);
   }
 };
