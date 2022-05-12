72c5216fbfeab33d491d600c17736374ef1dd587
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-12 14:33:01 +0300

    added checkAuthThunk and added statement if user role is not admin navigate to index page

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -1,10 +1,28 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import Header from '../../components/Header/Header';
-import Auth from '../../components/Auth/Auth';
 import AdminPanel from '../../components/AdminPanel/AdminPanel';
+import Auth from '../../components/Auth/Auth';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { checkAuthThunk } from '../../store';
+import Header from '../../components/Header/Header';
 
 const Admin: FC = () => {
+  const dispatch = useAppDispatch();
+  const { user, authStatus } = useAppSelector((state) => state.authReducer);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(checkAuthThunk());
+  }, []);
+
+  useEffect(() => {
+    if ((user?.role !== 'admin' && authStatus === 'fulfilled') || authStatus === 'rejected') {
+      navigate('../');
+    }
+  }, [authStatus, user?.role]);
+
   return (
     <div>
       <Header />
