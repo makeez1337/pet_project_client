7478214e0995931804fa74cff4042dc0ffa1e27f
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-12 22:50:10 +0300

    implemented new interface

--- src/interfaces/basketDeviceInterface.ts
+++ src/interfaces/basketDeviceInterface.ts
@@ -1,7 +1,15 @@
+import { IPhone } from './PhoneFieldsInterface';
+
 export interface IBasketDeviceResponse {
   id: number;
   phoneId: number;
   basketId: number;
   createdAt: string;
   updatedAt: string;
+}
+
+export interface IBasketDevicesCountResponse {
+  count: string;
+  totalPrice: number;
+  phone: IPhone;
 }
