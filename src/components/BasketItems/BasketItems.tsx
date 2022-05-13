1f898fd7a6ac018d9b237730fd41a3be7d343fd5
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-13 10:43:56 +0300

    added reduce to count allAmount of products and added state to re-render page if phone was deleted from basket

--- src/components/BasketItems/BasketItems.tsx
+++ src/components/BasketItems/BasketItems.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useEffect, useState } from 'react';
 
 import { useAppSelector } from '../../hooks';
+import { constants } from '../../constants';
 import { basketDeviceService } from '../../services/basketDeviceService';
 import { IBasketDevicesCountResponse } from '../../interfaces';
 import BasketItem from '../BasketItem/BasketItem';
@@ -10,13 +11,24 @@ const BasketItems: FC = () => {
   const { user, authStatus } = useAppSelector((state) => state.authReducer);
   const [basketItems, setBasketItems] = useState<IBasketDevicesCountResponse[] | []>([]);
 
+  const [allAmount, setAllAmount] = useState<number | null>(null);
+
+  const [isDeleted, setIsDeleted] = useState<null | boolean>(null);
+
   useEffect(() => {
     if (authStatus === 'fulfilled') {
-      basketDeviceService
-        .getDevicesByUserId(user?.id as number)
-        .then((res) => setBasketItems(res.data));
+      basketDeviceService.getDevicesByUserId(user?.id as number).then((res) => {
+        setBasketItems(res.data);
+
+        const allAmount = res.data.reduce((previousValue, currentValue) => {
+          return previousValue + (currentValue.totalPrice * Number(currentValue.count));
+        }, 0);
+        setAllAmount(allAmount);
+      });
     }
-  }, [authStatus]);
+  }, [authStatus, isDeleted]);
+
+  console.log(!!allAmount);
 
   return (
     <div className={css.header_wrap}>
@@ -24,7 +36,19 @@ const BasketItems: FC = () => {
         <h1 className={css.header}>КОРЗИНА:</h1>
       </div>
       <div className={css.basket_item_wrap}>
-        {basketItems && basketItems.map((value) => <BasketItem key={value.phone.id} {...value} />)}
+        {basketItems &&
+          basketItems.map((value) => (
+            <BasketItem key={value.phone.id} {...value} setIsDeleted={setIsDeleted} />
+          ))}
+        {!!allAmount && (
+          <div>
+            Загальна вартість:
+            <span className={css.clr_red}>
+               {allAmount.toString().replace(constants.numberWithCommas, ',')}
+            </span>{' '}
+            грн
+          </div>
+        )}
       </div>
     </div>
   );
