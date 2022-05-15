6ce89e65da015f645482cfb6df876c0913209eb7
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-15 14:16:45 +0300

    created function to delete phone if user is admin

--- src/components/Phone/Phone.tsx
+++ src/components/Phone/Phone.tsx
@@ -1,28 +1,50 @@
-import React, { FC } from 'react';
+import React, { Dispatch, FC, SetStateAction } from 'react';
 
 import { constants } from '../../constants';
 import { IPhone } from '../../interfaces';
+import { basketDeviceService, phoneService } from '../../services';
+import { useAppSelector } from '../../hooks';
+import delete_circle from '../../images/red-circle-delete.png';
 import css from './Phone.module.css';
-import { basketDeviceService } from '../../services/basketDeviceService';
 
-const Phone: FC<IPhone> = (props) => {
-  const { name, img, price, id } = props;
+interface PhoneProps extends IPhone {
+  setIsDeleted: Dispatch<SetStateAction<boolean | null>>;
+}
+
+const Phone: FC<PhoneProps> = (props) => {
+  const { name, img, price, id, setIsDeleted } = props;
+  const { user } = useAppSelector((prev) => prev.authReducer);
 
   const splitedImg = img.split('/')[3];
   const phoneImg = `${constants.baseImgUrl}${splitedImg}`;
 
   const addItem = async () => {
-    const response = await basketDeviceService.createBasketDevice(id).catch(e => {
+    const response = await basketDeviceService.createBasketDevice(id).catch((e) => {
       window.alert('Потрібно авторизуватись');
-    })
+    });
 
     if (response?.data) {
-      window.alert('Товар добавлений в корзину')
+      window.alert('Товар добавлений в корзину');
+    }
+  };
+
+  const deletePhone = async () => {
+    setIsDeleted(false);
+    try {
+      await phoneService.deleteById(id);
+      setIsDeleted(true);
+    } catch (e) {
+      setIsDeleted(false);
     }
   };
 
   return (
     <div className={css.content_wrap}>
+      {user?.role === 'admin' && (
+        <div>
+          <img onClick={deletePhone} className={css.dlt_circle} src={delete_circle} alt="" />
+        </div>
+      )}
       <div>
         <img className={css.phoneImg} src={phoneImg} alt="phone" />
       </div>
