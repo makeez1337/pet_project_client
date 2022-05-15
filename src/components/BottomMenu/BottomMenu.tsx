294eaf20b59b51f62daa21ac8d844190f43e3871
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-15 14:55:20 +0300

    created bottom menu

---
+++ src/components/BottomMenu/BottomMenu.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+
+import phoneImg from '../../images/phone_for_menu.png';
+import css from './BottomMenu.module.css';
+import {Link} from "react-router-dom";
+
+const BottomMenu: FC = () => {
+  return (
+    <div className={css.content_wrap}>
+      <div className={css.box}>
+        <div className={css.text_wrap}>
+          <span className={css.text_up}>
+            Смартфони
+          </span>
+          <span className={css.text_down}>
+            <Link to={'catalog'}>Переглянути всі</Link>
+          </span>
+        </div>
+        <div>
+          <img className={css.phoneImg} src={phoneImg} alt=""/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BottomMenu;
