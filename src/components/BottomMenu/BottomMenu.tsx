b7a71bd287bc8e6993cc31cccf80bbd18ba19837
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-15 15:46:39 +0300

    added symbol in some text

--- src/components/BottomMenu/BottomMenu.tsx
+++ src/components/BottomMenu/BottomMenu.tsx
@@ -1,23 +1,21 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 import phoneImg from '../../images/phone_for_menu.png';
 import css from './BottomMenu.module.css';
-import {Link} from "react-router-dom";
 
 const BottomMenu: FC = () => {
   return (
     <div className={css.content_wrap}>
       <div className={css.box}>
         <div className={css.text_wrap}>
-          <span className={css.text_up}>
-            Смартфони
-          </span>
+          <span className={css.text_up}>Смартфони</span>
           <span className={css.text_down}>
-            <Link to={'catalog'}>Переглянути всі</Link>
+            <Link to={'catalog'}>Переглянути всі {'>'} </Link>
           </span>
         </div>
         <div>
-          <img className={css.phoneImg} src={phoneImg} alt=""/>
+          <img className={css.phoneImg} src={phoneImg} alt="" />
         </div>
       </div>
     </div>
