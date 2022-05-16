7979bd6247b6bdd6ce4513c1e3474241ea788dbe
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-16 10:50:05 +0300

    added service to get min and max price of phones

--- src/services/phoneService.ts
+++ src/services/phoneService.ts
@@ -2,7 +2,7 @@ import axiosService from './axiosService';
 
 import { Urls } from '../constants';
 import { AxiosResponse } from 'axios';
-import {IPhone, IPhoneJoin, PhonePaginationResponse} from '../interfaces';
+import { IPhone, IPhoneJoin, IPhoneMinAndMax, PhonePaginationResponse } from '../interfaces';
 
 export const phoneService = {
   getByQuery: async (searchQuery: string): Promise<AxiosResponse<PhonePaginationResponse>> => {
@@ -11,10 +11,13 @@ export const phoneService = {
   create: async (data: FormData): Promise<AxiosResponse<IPhone>> => {
     return axiosService.post(Urls.Phones, data);
   },
-  deleteById: (id: number): Promise<AxiosResponse<number>> => {
+  deleteById: async (id: number): Promise<AxiosResponse<number>> => {
     return axiosService.delete(Urls.Phones, { data: { id } });
   },
-  getById: (id: number):Promise<AxiosResponse<IPhoneJoin>> => {
+  getById: async (id: number): Promise<AxiosResponse<IPhoneJoin>> => {
     return axiosService.get(`${Urls.Phones}/${id}`);
+  },
+  minAndMax: async (): Promise<AxiosResponse<IPhoneMinAndMax[]>> => {
+    return axiosService.get(`${Urls.Phones}/minAndMax`);
   }
 };
